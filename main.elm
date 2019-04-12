3592214410fdfb1bcb26ae5747893632123bfe0a
Aaron Sullivan <aasullivan1618@gmail.com>
2019-04-12 16:21:46 -0700

    Add basic signin UI

--- main.elm
+++ main.elm
@@ -2,7 +2,7 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (class, placeholder)
+import Html.Attributes exposing (attribute, autofocus, class, placeholder)
 import Html.Events exposing (onClick, onInput, onSubmit)
 import Http exposing (..)
 import Json.Encode as Encode
@@ -61,6 +61,64 @@ subscriptions model =
 -- VIEW
 
 
+searchBar : Html Msg
+searchBar =
+    nav [ class "level" ]
+        [ div [ class "level-item" ]
+            [ div [ class "field has-addons" ]
+                [ p [ class "control" ]
+                    [ input [ class "input", placeholder "Type stuff here" ] []
+                    ]
+                , p [ class "control" ]
+                    [ button [ class "button" ] [ text "Do Stuff" ]
+                    ]
+                ]
+            ]
+        ]
+
+
+navbar : Html Msg
+navbar =
+    nav [ class "navbar" ]
+        [ div [ class "navbar-brand" ]
+            [ a [ class "navbar-item" ] [ text "Invoice Generator" ]
+            ]
+        ]
+
+
+primaryInput : String -> Html Msg
+primaryInput pholder =
+    div [ class "field" ]
+        [ div [ class "control has-icons-left" ]
+            [ input [ class "input is-primary is-large", placeholder pholder, autofocus True ] []
+            , span [ class "icon is-large is-left" ]
+                [ i [ class "fas fa-envelope fa-xl" ] []
+                ]
+            ]
+        ]
+
+
+signinPage : Html Msg
+signinPage =
+    section [ class "hero is-success is-fullheight" ]
+        [ navbar
+        , div [ class "hero-body" ]
+            [ div [ class "container" ]
+                [ primaryInput "Username"
+                , primaryInput "Password"
+                , div [ class "field is-grouped level" ]
+                    [ p [ class "control level-item" ]
+                        [ button [ class "button is-primary is-large is-inverted" ] [ text "Sign In" ]
+                        ]
+                    , p [ class "control level-item" ]
+                        [ button [ class "button is-primary is-large is-inverted" ] [ text "Sign Up" ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+
+
 view : Model -> Html Msg
 view model =
-    div [ class "container" ] [ text "Hello, world" ]
+    signinPage
